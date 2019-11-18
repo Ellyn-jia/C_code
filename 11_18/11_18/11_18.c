c948f85a25cd200fe031f0c88d111d85009d3667
Ellyn-jia <15846706868@163.com>
2019-11-18 20:51:01 +0800

    modified

--- 11_18/11_18/11_18.c
+++ 11_18/11_18/11_18.c
@@ -3,30 +3,30 @@
 //ABCD左旋两个字符得到CDAB
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-void move(char* arr, int k)
-{
-	while (k--)
-	{
-		int start = 0;
-		int init = arr[start];
-		while (arr[start + 1])
-		{
-			arr[start] = arr[start + 1];
-			start++;
-		}
-		arr[start] = init;
-	}
-}
-
-int main()
-{
-	int k = 0;
-	char arr[] = "ABCD";
-	scanf("%d", &k);
-	move(arr, k);
-	printf("%s\n", arr);
-	return 0;
-}
+//void move(char* arr, int k)
+//{
+//	while (k--)
+//	{
+//		int start = 0;
+//		int init = arr[start];
+//		while (arr[start + 1])
+//		{
+//			arr[start] = arr[start + 1];
+//			start++;
+//		}
+//		arr[start] = init;
+//	}
+//}
+//
+//int main()
+//{
+//	int k = 0;
+//	char arr[] = "ABCD";
+//	scanf("%d", &k);
+//	move(arr, k);
+//	printf("%s\n", arr);
+//	return 0;
+//}
 
 
 
@@ -42,3 +42,28 @@ int main()
 //AABCD左旋两个字符得到BCDAA
 //
 //AABCD右旋一个字符得到DAABC
+
+#include<string.h>
+
+int check_arr(char* arr1, char* src)
+{
+	if (strstr(strncat(arr1, arr1,5), src) != NULL)
+	{
+		return 1;
+	}
+	else
+		return 0;
+}
+
+
+
+int main()
+{
+	char arr1[20];
+	char src[20];
+	scanf("%s", arr1);
+	scanf("%s", src);
+	check_arr(arr1,src);
+	printf("%d\n", check_arr(arr1,src));
+	return 0;
+}
