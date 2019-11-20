2162e47f34fa2b1372efa7dca0295257787c7b8d
Ellyn-jia <15846706868@163.com>
2019-11-20 17:41:15 +0800

    modified

--- 11_19/11_19/11_19.c
+++ 11_19/11_19/11_19.c
@@ -2,50 +2,50 @@
 //其他所有数字都出现了两次。
 //找出这两个只出现一次的数字，编程实现。
 //
-#define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
-void different_number(int* arr, int sz)
-{
-	int ret = 0;
-	int index = 0;
-	int num1 = 0, num2 = 0;
-	for (int i = 0; i < sz; i++)
-	{
-		ret ^= arr[i];
-	}
-	for (int i = 0; i < 32; i++)
-	{
-		if (((ret >> i) & 1) == 1)
-		{
-			index = i;
-		}
-	}
-	for (int i = 0; i < sz; i++)
-	{
-		if (((arr[i] >> index) & 1) == 1)
-			num1 ^= arr[i];
-		else
-			num2 ^= arr[i];
-	}
-	printf("%d %d \n", num1, num2);
-}
-
-int main()
-{
-	int arr[20];
-	int n = 0;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-	{
-		scanf("%d", &arr[i]);
-	}
-	different_number(arr, n);
-	return 0;
-}
-
-
-
-
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//void different_number(int* arr, int sz)
+//{
+//	int ret = 0;
+//	int index = 0;
+//	int num1 = 0, num2 = 0;
+//	for (int i = 0; i < sz; i++)
+//	{
+//		ret ^= arr[i];
+//	}
+//	for (int i = 0; i < 32; i++)
+//	{
+//		if (((ret >> i) & 1) == 1)
+//		{
+//			index = i;
+//		}
+//	}
+//	for (int i = 0; i < sz; i++)
+//	{
+//		if (((arr[i] >> index) & 1) == 1)
+//			num1 ^= arr[i];
+//		else
+//			num2 ^= arr[i];
+//	}
+//	printf("%d %d \n", num1, num2);
+//}
+//
+//int main()
+//{
+//	int arr[20];
+//	int n = 0;
+//	scanf("%d", &n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	different_number(arr, n);
+//	return 0;
+//}
+//
+//
+//
+//
 //2.喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，
 //给20元，可以多少汽水。
 //编程实现。
@@ -54,10 +54,30 @@ int main()
 //#include<stdio.h>
 //int main()
 //{
-//
+//	int empty = 20;
+//	int number = 20;
+//	while (empty > 1)
+//	{
+//		number += empty / 2;
+//		empty = empty / 2 + empty % 2;
+//	}
+//	printf("%d\n", number);
 //	return 0;
 //}
-//
+
 
 //3.模拟实现strcpy
+
+
+
+
+
+
 //4.模拟实现strcat
+
+
+
+
+
+
+
