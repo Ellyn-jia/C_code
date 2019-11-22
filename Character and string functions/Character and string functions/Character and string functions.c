8c0e4de1e8adf32f4a7c1fdb457f5952e5d6624b
Ellyn-jia <15846706868@163.com>
2019-11-22 22:46:10 +0800

    modified

--- Character and string functions/Character and string functions/Character and string functions.c
+++ Character and string functions/Character and string functions/Character and string functions.c
@@ -23,42 +23,70 @@
 //
 
 //2.实现strcat
-#include<stdio.h>
-char* Strcat(char* dest, const char* src)
-{
-	char* ret = dest;
-	while (*dest)
-	{
-		dest++;
-	}
-	while ((*dest++ = *src++))
-	{
-		;
-	}
-	return ret;
-}
-
-
-int main()
-{
-	char dest[9] = "ABCD";
-	char src[] = "EFGK";
-	printf("%s\n", Strcat(dest, src));
-	return 0;
-}
+//#include<stdio.h>
+//char* Strcat(char* dest, const char* src)
+//{
+//	char* ret = dest;
+//	while (*dest)
+//	{
+//		dest++;
+//	}
+//	while ((*dest++ = *src++))
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//
+//
+//int main()
+//{
+//	char dest[9] = "ABCD";
+//	char src[] = "EFGK";
+//	printf("%s\n", Strcat(dest, src));
+//	return 0;
+//}
 
 
 
 //3.实现strstr
 
-
-
-
-
-
-
-
-
+//#include<stdio.h>
+//#include<assert.h>
+//char* Strstr(const char* dest, const char* src)
+//{
+//	assert(dest && src);
+//	const char* desti = dest;
+//	const char* srci = src;
+//	while (*dest)
+//	{
+//		while (*desti == *srci && *srci != '\0')
+//		{
+//			desti++;
+//			srci++;
+//		}
+//		if (*srci == '\0')
+//		{
+//			return dest;
+//		}
+//		else
+//		{
+//			dest++;
+//			desti = dest;
+//			srci = src;
+//		}
+//	}
+//	return NULL;
+//
+//}
+//
+//int main()
+//{
+//	char dest[] = "ABCDEFGKGEGK";
+//	char src[] = "EFGK";
+//	printf("%s\n", Strstr(dest, src));
+//	return 0;
+//}
 
 
 
