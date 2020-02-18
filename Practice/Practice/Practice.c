9dc51bd8e3f50ccc43a7a795d9f699b4d51d377d
Ellyn-jia <15846706868@163.com>
2020-02-18 21:39:33 +0800

    2.18

---
+++ Practice/Practice/Practice.c
@@ -0,0 +1,73 @@
+char * toLowerCase(char * str){
+	char* p = str;
+	while (*p)
+	{
+		if ((*p) >= 'A' && (*p) <= 'Z')
+		{
+			(*p) += 32;
+		}
+		p++;
+	}
+	return str;
+}
+
+void reverse(int* nums, int start, int end)
+{
+	while (start < end)
+	{
+		int tmp = nums[start];
+		nums[start] = nums[end];
+		nums[end] = tmp;
+		end--;
+		start++;
+	}
+}
+
+void rotate(int* nums, int numsSize, int k){
+	k = k % numsSize;
+	reverse(nums, 0, numsSize - 1);
+	reverse(nums, 0, k - 1);
+	reverse(nums, k, numsSize - 1);
+}
+
+
+
+
+int removeElement(int* nums, int numsSize, int val){
+	int first1 = 0;
+	int first2 = 0;
+	while (first1 < numsSize)
+	{
+		if (nums[first1] == val)
+		{
+			first1++;
+		}
+		else
+		{
+			nums[first2] = nums[first1];
+			first2++;
+			first1++;
+		}
+	}
+	return first2;
+}
+
+
+
+int searchInsert(int* nums, int numsSize, int target){
+	int start = 0;
+	while (start < numsSize)
+	{
+		if (nums[start] < target)
+		{
+			start++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return start;
+}
+
+
