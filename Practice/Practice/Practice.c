de00bda9eb3df2cb3e39ec4bdc8b2a5b768d4c4a
Ellyn-jia <15846706868@163.com>
2020-02-20 22:23:55 +0800

    2.20

--- Practice/Practice/Practice.c
+++ Practice/Practice/Practice.c
@@ -96,3 +96,99 @@ bool canConstruct(char * ransomNote, char * magazine){
 		return false;
 	}
 }
+
+
+
+bool isPalindrome(int x){
+	if (x < 0 || (x % 10 == 0 && x != 0))
+	{
+		return false;
+	}
+	int number = 0;
+	while (x > number)
+	{
+		number = x % 10 + number * 10;
+		x = x / 10;
+	}
+	if (number == x || number / 10 == x)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+
+int lengthOfLastWord(char * s){
+	int len = strlen(s);
+	int i = len - 1;
+	int count = 0;
+	while (i != -1 && s[i] == ' ')
+	{
+		i--;
+	}
+	while (i != -1 && s[i] != ' ')
+	{
+		count++;
+		i--;
+	}
+	return count;
+}
+
+
+
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
+	int end1 = m - 1;
+	int end2 = n - 1;
+	int ends = m + n - 1;
+	while (end1 >= 0 && end2 >= 0)
+	{
+		if (nums1[end1] > nums2[end2])
+		{
+			nums1[ends] = nums1[end1];
+			end1--;
+			ends--;
+		}
+		else
+		{
+			nums1[ends] = nums2[end2];
+			end2--;
+			ends--;
+		}
+	}
+	if (end1 < 0)
+	{
+		while (end2 >= 0)
+		{
+			nums1[ends] = nums2[end2];
+			ends--;
+			end2--;
+		}
+	}
+}
+
+
+
+bool containsDuplicate(int* nums, int numsSize){
+	if (nums == NULL)
+		return false;
+	int start1 = 0;
+	int start2 = 0;
+	while (start1 < numsSize)
+	{
+		start2 = start1 + 1;
+		while ((start2 < numsSize) && (nums[start1] != nums[start2]))
+		{
+			start2++;
+		}
+		if (start2 == numsSize)
+			start1++;
+		else
+			return true;
+	}
+	return false;
+
+}
+
