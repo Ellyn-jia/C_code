94d5dabf961e5b1b0efd779be1967105f36a0dd9
Ellyn-jia <15846706868@163.com>
2020-02-26 18:01:03 +0800

    2.26

--- Practice/Practice/Practice.c
+++ Practice/Practice/Practice.c
@@ -519,3 +519,49 @@ int* searchRange(int* nums, int numsSize, int target, int* returnSize){
 }
 
 
+
+
+
+
+bool isPalindrome(char * s){
+	int end = strlen(s);
+	if (end == 0 || end == 1)
+		return true;
+	int start = 0;
+	while (start < end)
+	{
+		while ((start < end) && !((*(s + start) >= 'a' && *(s + start) <= 'z') ||
+			(*(s + start) >= 'A' && *(s + start) <= 'Z') ||
+			(*(s + start) >= '0' && *(s + start) <= '9')))
+			start++;
+		while ((start < end) && !((*(s + end) >= 'a' && *(s + end) <= 'z') ||
+			(*(s + end) >= 'A' && *(s + end) <= 'Z') ||
+			(*(s + end) >= '0' && *(s + end) <= '9')))
+			end--;
+		if (*(s + start) >= 'A' && *(s + start) <= 'Z')
+			*(s + start) += 32;
+		if (*(s + end) >= 'A' && *(s + end) <= 'Z')
+			*(s + end) += 32;
+		if (*(s + start) == *(s + end))
+		{
+			start++;
+			end--;
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+	return true;
+
+
+}
+
+
+
+
+
+
+
+
